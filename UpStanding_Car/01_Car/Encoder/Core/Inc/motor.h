7009ce8d3a8a406a7fb6f4ca8587011660b021b3
tcy7 <TNo0MJQI1BEtihaXxFHa@outlook.com>
2025-10-05 10:34:27 +0800

    01_MDK_HAL

---
+++ UpStanding_Car/01_Car/Encoder/Core/Inc/motor.h
@@ -0,0 +1,8 @@
+#ifndef __MOTOR_H
+#define __MOTOR_H
+
+#include "stm32f1xx_hal.h"
+
+void Load(int moto1,int moto2);			//-7200~7200
+
+#endif 
---
+++ UpStanding_Car/01_Car/HC-SR04/Core/Inc/sr04.h
@@ -0,0 +1,9 @@
+#ifndef _SR04_H
+#define _SR04_H
+
+#include "stm32f1xx_hal.h"
+
+
+void GET_Distance(void);
+
+#endif
